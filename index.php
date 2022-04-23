8acb42ff50ecc4d5b151fe183d615f9e3dbb2e1a
Lucas Dionisi <lucas.dionisi@gmail.com>
2022-04-23 10:20:26 +0200

    Add: Title in header

--- index.php
+++ index.php
@@ -4,5 +4,13 @@
     <?php include 'tools/head.php';?>
   </head>
   <body>
+    <header>
+      <div class="header-left">
+        <p>Notation de bière</p>
+      </div>
+      <div class="header-right">
+        <p>Image de profil</p>
+      </div>
+    </header>
   </body>
 </html>
