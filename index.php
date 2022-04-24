3a1c243a4010e21545304a72192b0565b01b7953
Lucas Dionisi <lucas.dionisi@gmail.com>
2022-04-24 23:41:28 +0200

    Add: Display div to add an advice

--- index.php
+++ index.php
@@ -7,10 +7,14 @@
   <body>
     <header>
       <div class="header header-left">
-        <img src="resources/img/clinking-beer.svg" alt="Logo notation de bière"/>
+        <img src="resources/img/beer.svg" alt="Logo notation de bière"/>
         <p>Notation de bière</p>
       </div>
       <div class="header header-right">
+        <div class="add-advice">
+          <img src="resources/img/add.svg" alt="Ajouter un avis"/>
+          <p>Ajouter un avis</p>
+        </div>
         <img src="resources/img/profil.svg" alt="Photo de profil"/>
       </div>
     </header>
