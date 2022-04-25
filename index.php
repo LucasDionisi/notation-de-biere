e80f245c7bd2365559f3ac34b9023fa1ab555453
Lucas Dionisi <lucas.dionisi@gmail.com>
2022-04-25 20:49:55 +0200

    Fix: Separate header in file

--- index.php
+++ index.php
@@ -5,19 +5,7 @@
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
-    <header>
-      <div class="header header-left">
-        <img src="resources/img/beer.svg" alt="Logo notation de bière"/>
-        <p>Notation de bière</p>
-      </div>
-      <div class="header header-right">
-        <div class="add-advice">
-          <img src="resources/img/add.svg" alt="Ajouter un avis"/>
-          <p>Ajouter un avis</p>
-        </div>
-        <img src="resources/img/profil.svg" alt="Photo de profil"/>
-      </div>
-    </header>
+  <?php include 'tools/header.php';?>
     <main>
       <form class="form-search" role="search">
         <input type="search" class="input-search" placeholder="Quelle bière ?" aria-label="Rechercher une bière">
---
+++ tools/header.php
@@ -0,0 +1,13 @@
+<header>
+      <div class="header header-left">
+        <img src="resources/img/beer.svg" alt="Logo notation de bière"/>
+        <p>Notation de bière</p>
+      </div>
+      <div class="header header-right">
+        <div class="add-advice">
+          <img src="resources/img/add.svg" alt="Ajouter un avis"/>
+          <p>Ajouter un avis</p>
+        </div>
+        <img src="resources/img/profil.svg" alt="Photo de profil"/>
+      </div>
+    </header>
