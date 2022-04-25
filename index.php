5c2b54c1565c49b916416a79984ba6fa85d8f45e
Lucas Dionisi <lucas.dionisi@gmail.com>
2022-04-25 20:51:05 +0200

    Fix: Move link style.css

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <html lang="fr">
   <head>
     <?php include 'tools/head.php';?>
-    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
   <?php include 'tools/header.php';?>
--- tools/head.php
+++ tools/head.php
@@ -2,6 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>Notation de bière</title>
+<link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="../css/header.css">
 <link rel="stylesheet" href="../css/main.css">
 <link rel="stylesheet" href="../css/home.css">
