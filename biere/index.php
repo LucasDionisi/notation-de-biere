9d6be152a7b6ee51b579ea8bc9835a38e616aaf1
Lucas DIONISI <lucas.dionisi@3ds.com>
2022-04-29 14:20:13 +0200

    Add title and rate to beer description

--- biere/index.php
+++ biere/index.php
@@ -2,8 +2,21 @@
 <html lang="fr">
   <head>
     <?php include '../tools/head.php';?>
+    <link rel="stylesheet" href="../css/beer.css">
   </head>
   <body>
     <?php include '../tools/header.php';?>
+    <div class="beer-info">
+      <div class="title-and-rate">
+        <h1>Fada</h1>
+        <div class="rate-icons">
+          <img src="../resources/img/beer-rate.svg">
+          <img src="../resources/img/beer-rate.svg">
+          <img src="../resources/img/beer-rate.svg">
+          <img src="../resources/img/beer-rate.svg">
+          <img src="../resources/img/beer-rate.svg">
+        </div>
+      </div>
+    </div>
   </body>
 </html>
