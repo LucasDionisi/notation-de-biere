6e0634cad23909a21f559f7a5164a7e68d3274db
Lucas DIONISI <lucas.dionisi@3ds.com>
2022-04-29 15:29:47 +0200

    Add: beer informations

--- biere/index.php
+++ biere/index.php
@@ -17,6 +17,12 @@
           <img src="../resources/img/beer-rate.svg">
         </div>
       </div>
+      <div class="description-and-img">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <img src="../resources/img/beers/fada.png">
+      </div>
     </div>
   </body>
 </html>
