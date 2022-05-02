5f385a600879d36c5c1fddd4c409f5abded4af64
Lucas Dionisi <lucas.dionisi@gmail.com>
2022-05-02 21:01:19 +0200

    Add: new page to add a beer

---
+++ ajouter/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <?php include '../tools/head.php';?>
+    <link rel="stylesheet" href="../css/add.css">
+  </head>
+  <body>
+    <?php include '../tools/header.php';?>
+  </body>
+</html>
--- tools/header.php
+++ tools/header.php
@@ -1,13 +1,15 @@
 <header>
-      <div class="header header-left">
-        <img src="../resources/img/beer.svg" alt="Logo notation de bière"/>
-        <p>Notation de bière</p>
-      </div>
-      <div class="header header-right">
-        <div class="add-beer">
-          <img src="../resources/img/add.svg" alt="Ajouter un avis"/>
-          <p>Ajouter une bière</p>
-        </div>
-        <img src="../resources/img/profil.svg" alt="Photo de profil"/>
-      </div>
-    </header>
+  <div class="header header-left">
+    <img src="../resources/img/beer.svg" alt="Logo notation de bière"/>
+    <p>Notation de bière</p>
+  </div>
+  <div class="header header-right">
+    <div class="add-beer">
+      <a href="../ajouter/">
+        <img src="../resources/img/add.svg" alt="Ajouter un avis"/>
+        <p>Ajouter une bière</p>
+      </a>
+    </div>
+    <img src="../resources/img/profil.svg" alt="Photo de profil"/>
+  </div>
+</header>
