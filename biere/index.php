e73bef0139edc7e552491659e626dfe45a11f29e
Lucas DIONISI <lucas.dionisi@3ds.com>
2022-05-04 16:09:57 +0200

    Add advices

--- biere/index.php
+++ biere/index.php
@@ -6,40 +6,42 @@
   </head>
   <body>
     <?php include '../tools/header.php';?>
-    <div class="beer-info">
-      <div class="title-and-rate">
-        <h1>Fada</h1>
-        <div class="rate-icons">
-          <img src="../resources/img/beer-rate.svg">
-          <img src="../resources/img/beer-rate.svg">
-          <img src="../resources/img/beer-rate.svg">
-          <img src="../resources/img/beer-rate.svg">
-          <img src="../resources/img/beer-rate.svg">
+    <div class="beer-page">
+      <div class="beer-info">
+        <div class="title-and-rate">
+          <h1>Fada</h1>
+          <div class="rate-icons">
+            <img src="../resources/img/beer-rate.svg">
+            <img src="../resources/img/beer-rate.svg">
+            <img src="../resources/img/beer-rate.svg">
+            <img src="../resources/img/beer-rate.svg">
+            <img src="../resources/img/beer-rate.svg">
+          </div>
+        </div>
+        <div class="description-and-img">
+          <p>
+            Des matières premières sélectionnées avec grand soin
+
+            C’est à la Brasserie du Castellet à Signes que sont brassées ces bières artisanales et provençales, à partir de matières premières locales.
+
+            Epeautre tout droit venu de la Drôme pour la Blonde, romarin des collines cueilli à la main pour la blanche et saveur pêche pour la Sunny India Pale Ale.
+
+            Des recettes originales élaborées avec soin par Victor, maître brasseur amoureux de la Provence et passionné de son métier.
+          </p>
+          <img src="../resources/img/beers/fada.png">
         </div>
       </div>
-      <div class="description-and-img">
-        <p>
-          Des matières premières sélectionnées avec grand soin
-
-          C’est à la Brasserie du Castellet à Signes que sont brassées ces bières artisanales et provençales, à partir de matières premières locales.
-
-          Epeautre tout droit venu de la Drôme pour la Blonde, romarin des collines cueilli à la main pour la blanche et saveur pêche pour la Sunny India Pale Ale.
-
-          Des recettes originales élaborées avec soin par Victor, maître brasseur amoureux de la Provence et passionné de son métier.
-        </p>
-        <img src="../resources/img/beers/fada.png">
-      </div>
-    </div>
-    <div class="beer-content">
-      <button class="add-advice">Rédiger un avis</button>
-      <div class="beer-advices">
-        <div class="beer-advice"></div>
-        <div class="beer-advice"></div>
-        <div class="beer-advice"></div>
-        <div class="beer-advice"></div>
-        <div class="beer-advice"></div>
-        <div class="beer-advice"></div>
-        <div class="beer-advice"></div>
+      <div class="beer-content">
+        <button class="add-advice">Rédiger un avis</button>
+        <div class="beer-advices">
+          <div class="beer-advice"></div>
+          <div class="beer-advice"></div>
+          <div class="beer-advice"></div>
+          <div class="beer-advice"></div>
+          <div class="beer-advice"></div>
+          <div class="beer-advice"></div>
+          <div class="beer-advice"></div>
+        </div>
       </div>
     </div>
   </body>
