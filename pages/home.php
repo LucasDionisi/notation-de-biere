743983c7bf6215f269ccbb861e38ee5c5bb2dd60
Lucas Dionisi <lucas.dionisi@gmail.com>
2022-05-09 22:33:35 +0200

    Change beer to be clickable to redirect page

--- pages/home.php
+++ pages/home.php
@@ -11,25 +11,27 @@
       </form>
       <div class="collection-beer">
         <div class="beer-preview">
-          <div class="beer-thumbnail">
-            <img src="resources/img/beers/leffe.jpg"/>
-          </div>
-          <div class="beer-description">
-            <div class="beer-description-name"><p>Bla bla bla</p></div>
-            <div class="beer-rate">
-              <div class="beer-rate-img">
-                <img src="resources/img/beer-rate.svg">
-                <img src="resources/img/beer-rate.svg">
-                <img src="resources/img/beer-rate.svg">
-                <img src="resources/img/beer-rate.svg">
-                <img src="resources/img/beer-rate.svg">
-              </div>
-              <div class="beer-rate-advices">
-                641
+          <a href="/biere">
+            <div class="beer-thumbnail">
+              <img src="resources/img/beers/leffe.jpg"/>
+            </div>
+            <div class="beer-description">
+              <div class="beer-description-name"><p>Bla bla bla</p></div>
+              <div class="beer-rate">
+                <div class="beer-rate-img">
+                  <img src="resources/img/beer-rate.svg">
+                  <img src="resources/img/beer-rate.svg">
+                  <img src="resources/img/beer-rate.svg">
+                  <img src="resources/img/beer-rate.svg">
+                  <img src="resources/img/beer-rate.svg">
+                </div>
+                <div class="beer-rate-advices">
+                  641
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </a>
         <div class="beer-preview">
           <div class="beer-thumbnail">
             <img src="resources/img/beers/fada.png"/>
