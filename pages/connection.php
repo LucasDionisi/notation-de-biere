c1364063e0f234cc308d2b43438a5054808b057d
Lucas Dionisi <lucas.dionisi@gmail.com>
2022-06-11 20:08:41 +0200

    Display error msg if email format is bad

--- pages/connection.php
+++ pages/connection.php
@@ -5,22 +5,46 @@
     <link rel="stylesheet" type="text/css" href="../css/connection.css">
   </head>
   <body>
-    <?php include 'includes/header.php';?>
+    <?php 
+      include 'includes/header.php';
+
+      if (isset($_POST['submitButton'])) {
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        $errorMsg = "";
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+          $errorMsg = "Le format de l'adresse mail n'est pas valide.";
+        } else {
+
+        }
+      }
+    ?>
     <div class="connection-page">
       <h1>Connexion</h1>
-      <div class="connection-box">
-        <div class="input-group">
-          <input id="email-input" type="text" name="email" required>
-          <span class="bar"></span>
-          <label>Adresse mail</label>
+      <form method="POST" action="">
+        <div class="connection-box">
+          <div class="input-group">
+            <input id="email-input" type="text" name="email" required>
+            <span class="bar"></span>
+            <label>Adresse mail</label>
+          </div>
+          <div class="input-group">
+            <input id="password-input" type="password" name="password" required>
+            <span class="bar"></span>
+            <label>Mot de passe</label>
+          </div>
+          <button type="submit" name="submitButton">Se connecter</button>
+          <?php
+            if (!empty($errorMsg)) {
+          ?>
+            <p class="error-message"><?=$errorMsg?></p>
+          <?php
+            }
+          ?>
         </div>
-        <div class="input-group">
-          <input id="password-input" type="password" name="password" required>
-          <span class="bar"></span>
-          <label>Mot de passe</label>
-        </div>
-        <button type="submit" name="submitButton">Se connecter</button>
-      </div>
+      </form>
       <p>Vous n'avez pas de compte ? <a href="../inscription">S'inscrire</a></p>
     </div>
   </body>
