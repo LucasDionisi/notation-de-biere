e96370f1eb537c4ff03b504cc8fe99cc9ab18f06
Lucas DIONISI <lucas.dionisi@3ds.com>
2022-06-23 16:21:25 +0200

    Fix beer info style

--- includes/beerInfo.php
+++ includes/beerInfo.php
@@ -26,35 +26,35 @@ try {
 
 <div class="beer-info">
   <div class="title-rate-and-description">
+    <div class="rate-icons">
+      <?php
+      if ($beer['rate_average']) {
+        $floor = floor($beer['rate_average']);
+        $i = 0;
+        while ($i < $floor) { ?>
+          <img src="../resources/img/beer-rate/beer-100.png">
+        <?php
+          $i++;
+        }
+        if ($i < 5) {
+        ?>
+          <img src="../resources/img/beer-rate/beer-<?= (int)((fmod($beer['rate_average'], $floor)) * 4) * 25 ?>.png">
+        <?php
+          $i++;
+        }
+        while ($i < 5) {
+        ?>
+          <img src="../resources/img/beer-rate/beer-0.png">
+      <?php
+          $i++;
+        }
+      }
+      ?>
+    </div>
     <div class="title-and-rate">
       <div class="title-info">
         <h1><?= $beer['name'] ?></h1>
         <p>Alc. <?= $beer['alcohol_level'] ?>% Vol.</p>
-      </div>
-      <div class="rate-icons">
-        <?php
-        if ($beer['rate_average']) {
-          $floor = floor($beer['rate_average']);
-          $i = 0;
-          while ($i < $floor) { ?>
-            <img src="../resources/img/beer-rate/beer-100.png">
-          <?php
-            $i++;
-          }
-          if ($i < 5) {
-          ?>
-            <img src="../resources/img/beer-rate/beer-<?= (int)((fmod($beer['rate_average'], $floor)) * 4) * 25 ?>.png">
-          <?php
-            $i++;
-          }
-          while ($i < 5) {
-          ?>
-            <img src="../resources/img/beer-rate/beer-0.png">
-        <?php
-            $i++;
-          }
-        }
-        ?>
       </div>
     </div>
     <div class="description">
