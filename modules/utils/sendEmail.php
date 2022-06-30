993aadb2495f848058e0ad75995f767ead9bdf2d
Lucas DIONISI <lucas.dionisi@3ds.com>
2022-06-30 12:39:56 +0200

    Fix message

--- modules/utils/sendEmail.php
+++ modules/utils/sendEmail.php
@@ -2,8 +2,6 @@
 
 	class SendEmail {
 
-		private $subject;
-		private $message;
 		private $header;
 
 		function __construct() {
@@ -14,19 +12,19 @@
 
 		private function getRegisterValidationMessage($userPseudo, $link) {
 			$message  = "<html>";
-			$message .= 	"<p>Salut {$userPseudo},</p><br>";
-			$message .=		"<p>Merci d'avoir rejoint Notabiere 🍻!</p><br><br>";
-			$message .=		"<p>Nous aimerions vous confirmer que votre compte a été créé avec succès. Pour activer votre compte, cliquez sur le lien ci-dessous.</p><br>";
-			$message .=		"<p>{$link}</p><br><br>";
-			$message .=		"<p>Si vous rencontrez des difficultés pour vous connecter à votre compte, contactez-nous à contact@notabiere.fr</p><br><br>";
-			$message .=		"<p>Cordialement,</p><br>";
+			$message .= 	"<p>Salut {$userPseudo},</p>";
+			$message .=		"<p>Merci d'avoir rejoint Notabiere 🍻!</p><br>";
+			$message .=		"<p>Nous aimerions vous confirmer que votre compte a été créé avec succès. Pour activer votre compte, cliquez sur le lien ci-dessous.</p>";
+			$message .=		"<p>{$link}</p><br>";
+			$message .=		"<p>Si vous rencontrez des difficultés pour vous connecter à votre compte, contactez-nous à contact@notabiere.fr</p><br>";
+			$message .=		"<p>Cordialement,</p>";
 			$message .=		"<p>L'équipe de Notabiere</p>";
 			$message .= "</html>";
 
 			return $message;
 		}
 
-		public function sendRegisterValidation($to, ,$userPseudo, $link) {
+		public function sendRegisterValidation($to, $userPseudo, $link) {
 			$subject = "Validation de votre adresse email";
 			$message = $this->getRegisterValidationMessage($userPseudo, $link);
 
