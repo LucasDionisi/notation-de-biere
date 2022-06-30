66463eab162b1fe88188afd754b5ca6153023375
Lucas DIONISI <lucas.dionisi@3ds.com>
2022-06-30 16:50:50 +0200

    Create page to create new beer

--- pages/add.php
+++ pages/add.php
@@ -3,6 +3,7 @@
 
 <head>
     <?php include 'includes/head.php'; ?>
+    <link rel="stylesheet" type="text/css" href="../css/<?=$cssAdd?>">
 </head>
 
 <body>
@@ -13,9 +14,26 @@
         header('Location: ../connexion');
       }
     ?>
-    <h1>
-        Ajouter une nouvelle bière
-    </h1>
+    <div class="page-content">
+        <h1>Ajouter une nouvelle bière</h1>
+        <div class="box-new">
+            <div class="left-box">
+                <div class="name-group">
+                    <input id="name-input" type="text" name="name" required maxlength="100">
+                    <span class="bar"></span>
+                    <label>Nom de la bière</label>
+                </div>
+                <div class="description-group">
+                    <textarea id="description-textarea" name="description" required maxlength="500"
+                        placeholder="Donnez une description à cette bière." rows="10" cols="40"></textarea>
+                </div>
+            </div>
+            <div class="right-box">
+                <img src="../resources/img/beers/fada.png">
+            </div>
+
+        </div>
+    </div>
 </body>
 
 </html>
