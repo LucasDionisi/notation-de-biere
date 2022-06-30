31c6301d558c998eb8745f566fb481e196fb27ec
Lucas DIONISI <lucas.dionisi@3ds.com>
2022-06-30 12:32:19 +0200

    Create message to vidate registration

--- modules/utils/sendEmail.php
+++ modules/utils/sendEmail.php
@@ -12,10 +12,25 @@
 			$this->header .= "Content-Type: text/html; charset=iso-8859-1\r\n";
 		}
 
-		public function sendRegisterValidation($to) {
-			$this->subject = "Validation de votre adresse email";
+		private function getRegisterValidationMessage($userPseudo, $link) {
+			$message  = "<html>";
+			$message .= 	"<p>Salut {$userPseudo},</p><br>";
+			$message .=		"<p>Merci d'avoir rejoint Notabiere 🍻!</p><br><br>";
+			$message .=		"<p>Nous aimerions vous confirmer que votre compte a été créé avec succès. Pour activer votre compte, cliquez sur le lien ci-dessous.</p><br>";
+			$message .=		"<p>{$link}</p><br><br>";
+			$message .=		"<p>Si vous rencontrez des difficultés pour vous connecter à votre compte, contactez-nous à contact@notabiere.fr</p><br><br>";
+			$message .=		"<p>Cordialement,</p><br>";
+			$message .=		"<p>L'équipe de Notabiere</p>";
+			$message .= "</html>";
 
-			mail($to, $this->subject, $this->message, $this->header);
+			return $message;
+		}
+
+		public function sendRegisterValidation($to, ,$userPseudo, $link) {
+			$subject = "Validation de votre adresse email";
+			$message = $this->getRegisterValidationMessage($userPseudo, $link);
+
+			mail($to, $subject, $message, $this->header);
 		}
 
 	}
