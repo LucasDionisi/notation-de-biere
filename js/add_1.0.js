644313fb6c09effe08532a523c4080de16e93c2e
Lucas DIONISI <lucas.dionisi@3ds.com>
2022-07-01 17:55:43 +0200

    fix style of page

--- js/add_1.0.js
+++ js/add_1.0.js
@@ -8,8 +8,8 @@ function selectImg() {
 $('#file-input').on('change', function() {
 
 	new Compressor(this.files[0], {
-		maxWidth: 800,
-		maxHeight: 800,
+		maxWidth: 500,
+		maxHeight: 500,
 		success:  function (compressedFile) {
 			document.getElementById('uploaded-img').src = window.URL.createObjectURL(compressedFile);
 		}
