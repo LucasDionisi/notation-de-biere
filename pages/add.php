3f8497c756f49596b3a40622b66129c85ec17823
Lucas DIONISI <lucas.dionisi@3ds.com>
2022-07-01 15:34:34 +0200

    Change design off add page

--- pages/add.php
+++ pages/add.php
@@ -17,21 +17,26 @@
     <div class="page-content">
         <h1>Ajouter une nouvelle bière</h1>
         <div class="box-new">
-            <div class="left-box">
-                <div class="name-group">
-                    <input id="name-input" type="text" name="name" required maxlength="100">
-                    <span class="bar"></span>
-                    <label>Nom de la bière</label>
+            <div class="box-main">
+                <div class="left-box">
+                    <div class="name-group">
+                        <input id="name-input" type="text" name="name" required maxlength="100">
+                        <span class="bar"></span>
+                        <label>Nom de la bière</label>
+                    </div>
+                    <div class="description-group">
+                        <textarea id="description-textarea" name="description" required maxlength="500"
+                            placeholder="Donnez une description à cette bière." rows="10" cols="40"></textarea>
+                    </div>
                 </div>
-                <div class="description-group">
-                    <textarea id="description-textarea" name="description" required maxlength="500"
-                        placeholder="Donnez une description à cette bière." rows="10" cols="40"></textarea>
+                <div class="right-box">
+                    <img id="uploaded-img" src="../resources/img/beers/fada.png">
+                    <button id="upload-img">Photo</button>
                 </div>
             </div>
-            <div class="right-box">
-                <img src="../resources/img/beers/fada.png">
+            <div>
+                <button class="submitButton">Publier</button>
             </div>
-
         </div>
     </div>
 </body>
