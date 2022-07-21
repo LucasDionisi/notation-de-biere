83544f30f6abbb83b389813a01173f1e04bc7698
Lucas DIONISI <lucas.dionisi@3ds.com>
2022-07-21 11:28:27 +0200

    change meta data

--- includes/head.php
+++ includes/head.php
@@ -1,6 +1,9 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="author" content="Lucas Dionisi">
+<meta name="description" content="Sélectionnez les meilleures bières, lisez des avis et notez vous-même n'importe quelle bière.">
+<meta name="keywords" content="notabiere, biere, avis, notation, note">
 <title>Notabière</title>
 <?php require_once 'modules/utils/fileVersionning.php'; ?>
 <link rel="stylesheet" href="../css/<?=$cssStyle?>">
